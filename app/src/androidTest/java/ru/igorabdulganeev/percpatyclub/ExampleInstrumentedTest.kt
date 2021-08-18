0a3c3835f8f9b3302749ed526aabd90fe18a8211
gorinih <gorinih@gmail.com>
2021-08-18 21:27:32 +0700

    rename package name

--- app/src/androidTest/java/ru/igorabdulganeev/percpatyclub/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/igorabdulganeev/percpatyclub/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ru.igorabdulganeev.myapplication
+package ru.igorabdulganeev.percpatyclub
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- app/src/main/java/ru/igorabdulganeev/percpatyclub/MainActivity.kt
+++ app/src/main/java/ru/igorabdulganeev/percpatyclub/MainActivity.kt
@@ -1,4 +1,4 @@
-package ru.igorabdulganeev.myapplication
+package ru.igorabdulganeev.percpatyclub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
